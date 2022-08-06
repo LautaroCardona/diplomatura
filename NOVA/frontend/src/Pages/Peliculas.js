669bceab1148ebb9811de1f319d23eb8b633dd16
lautaro <lautarouriellcardona@gmail.com>
2022-08-06 02:21:58 -0300

    NOVA

---
+++ NOVA/frontend/src/Pages/Peliculas.js
@@ -0,0 +1,14 @@
+const Peliculas = (props) => {
+    return (
+        <body>
+<div id="myDIV">
+<div class="container">
+<h2>Halo: La serie</h2>
+<a href="https://www.youtube.com/watch?v=u7oRIRn3mc4"> <img src="imagenes/ventana_Mesa de trabajo 1.jpg"></img> </a>
+</div>
+</div>
+</body>
+    );
+}
+export default Peliculas;
+
