5626db47bf263f041b121fb76f6a4379f34a9f03
lautaro <lautarouriellcardona@gmail.com>
2022-08-06 00:42:29 -0300

    NOVA

--- NOVA/frontend/src/Pages/Index.js
+++ NOVA/frontend/src/Pages/Index.js
@@ -1,7 +1,28 @@
-import React from 'react';
 const Index = (props) => {
     return (
-        <div>Index</div>
+        <body>
+  <div class="pantalla">
+
+    <div class="slider">
+
+      <div class="contexto">
+
+        <h1>¿Qué es la ciencia ficción?&nbsp;</h1>
+        <p>La ciencia ficción es un género cuyos contenidos se encuentran basados en supuestos logros científicos o
+          técnicos que podrían lograrse en el futuro. Este sustento científico hace que la ciencia ficción se diferencie
+          del género fantástico, donde las situaciones y los personajes son fruto de la imaginación. Dadas las
+          características mencionadas. De hecho, muchos autores de ciencia ficción han logrado anticipar el surgimiento
+          de distintos inventos, como Julio Verne con los submarinos o las naves espaciales.</p>
+
+        <p>En este sitio web podrás
+          acceder a una comunidad de fanaticos del genero, Intercambiar opiniones, Subir reseñas, y disfrutar de la
+          difución de las mejores historias.</p>
+
+        <div class="boton"><a href="https://www.fabulantes.com/">Ver más&nbsp;</a></div>
+      </div>
+    </div>
+  </div>
+</body>
     );
 }
 export default Index;
--- NOVA/frontend/src/components/layout/Footer.js
+++ NOVA/frontend/src/components/layout/Footer.js
@@ -1,7 +1,24 @@
 import React from 'react';
 const Footer = (props) => {
     return (
-        <div>Footer</div>
+        <footer>
+             <div class="col1">
+    <h1>INFORMACIÓN&nbsp;</h1>
+    <ul>
+      <li> Curso de programación</li>
+      <li>E-learning</li>
+      <li>UTN</li>
+      <li> LAUTARO CARDONA</li>
+    </ul>
+  </div>
+  <div class="col2">
+    <h1></h1>
+    <li> </li>
+    <li></li>
+    <li></li>
+    <li>&nbsp;</li>
+  </div>
+           </footer>
     );
 }
 export default Footer;
