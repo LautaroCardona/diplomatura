a61fe96749ae8da5616f9ec60c08c726ed9a83c6
lautaro <lautarouriellcardona@gmail.com>
2022-08-15 02:00:44 -0300

    NOVA

--- NOVA/frontend/src/components/layout/Nav.js
+++ NOVA/frontend/src/components/layout/Nav.js
@@ -1,15 +1,17 @@
-import { Link } from "react-router-dom";
+
+//import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const Nav = (props) => {
     return (
         <nav >
             <div>
                 <ul>
-                    <li><Link to="/">Index</Link></li>  
-                    <li><Link to="/Literatura">Literatura</Link></li>
-                    <li><Link to="/Peliculas">Peliculas</Link></li>
-                    <li><Link to="/Comics">Comics</Link></li>
-                    <li><Link to="/Contactos">Contactos</Link></li>
+                    <li><NavLink to="/" className={({isActive}) => isActive ? "activo": undefined}>Index</NavLink></li> 
+                    <li><NavLink to="/Literatura" className={({isActive}) => isActive ? "activo": undefined}>Literatura</NavLink></li>
+                    <li><NavLink to="/Peliculas" className={({isActive}) => isActive ? "activo": undefined}>Peliculas</NavLink></li>
+                    <li><NavLink to="/Comics" className={({isActive}) => isActive ? "activo": undefined}>Comics</NavLink></li>
+                    <li><NavLink to="/Contactos" className={({isActive}) => isActive ? "activo": undefined}>Contactos</NavLink></li>
                 </ul>
 
             </div>
